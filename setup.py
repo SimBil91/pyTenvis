d9c8d36bf1331c469c69424be6e8497b8c7095d5
Simon Bilgeri <Simon.Bilgeri@tum.de>
2015-05-28 22:02:51 +0200

    cleanup

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from distutils.core import setup
+import setuptools
 import pyTenvis
 
 setup( name='pyTenvis',
     version=pyTenvis.__version__,
-    maintainer='Simon Bilgeri',
-    maintainer_email='Simon.Bilgeri@gmx.de',
-    packages=find_packages(),
+    author = 'Simon Bilgeri',
+    author_email = 'Simon.Bilgeri@tum.de',
     include_package_data=True,
-    scripts=[],
     url='https://github.com/SimBil91/pyTenvis',
     download_url = 'https://github.com/SimBil91/pyTenvis/tarball/0.1',
     description='Library for Tenvis HD IPCAMs',
-      )
+    keywords = ['TENVIS', 'IPCAM', 'library'])
